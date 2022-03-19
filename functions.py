b22fc302358c0766491bbab574a1bd1684815ac9
Abid Chowdhury <58383484+Abid1732@users.noreply.github.com>
2022-03-19 02:33:43 -0400

    set color to green and reset color to gray after before each guess

--- functions.py
+++ functions.py
@@ -53,15 +53,50 @@ class UIFunctions(MainWindow):
             UIFunctions.set_yellow(self, self.ui.label_previous_guess_5)
 
     # set label to green
-    def check_if_green(self, guess, letters_in_word):
-        pass
+    def set_green(self, label):
+        label.setStyleSheet("""
+                             QLabel {
+                                 color: rgb(0, 255, 0);
+                                 font: 40px "Adam Bold";
+                                 background-color: rgba(0, 0, 0,0);
+                             }
+                             """)           
+    
+    def check_if_green(self, guess, word):
+        if guess[0] == word[0]:
+            UIFunctions.set_green(self, self.ui.label_previous_guess_1)
+        if guess[1] == word[1]:
+            UIFunctions.set_green(self, self.ui.label_previous_guess_2)
+        if guess[2] == word[2]:
+            UIFunctions.set_green(self, self.ui.label_previous_guess_3)
+        if guess[3] == word[3]:
+            UIFunctions.set_green(self, self.ui.label_previous_guess_4)
+        if guess[4] == word[4]:
+            UIFunctions.set_green(self, self.ui.label_previous_guess_5)
         
+    # reset colors before each guess
+    def set_gray(self, label):
+        label.setStyleSheet("""
+                             QLabel {
+                                 color: rgb(150, 150, 150);
+                                 font: 40px "Adam Bold";
+                                 background-color: rgba(0, 0, 0,0);
+                             }
+                             """)
+    
+    def reset_colors(self):
+        UIFunctions.set_gray(self, self.ui.label_previous_guess_1)
+        UIFunctions.set_gray(self, self.ui.label_previous_guess_2)
+        UIFunctions.set_gray(self, self.ui.label_previous_guess_3)
+        UIFunctions.set_gray(self, self.ui.label_previous_guess_4)
+        UIFunctions.set_gray(self, self.ui.label_previous_guess_5)
+    
     # get the guess
     def get_guess(self):
-        global letters_in_word
         guess = self.ui.entry_guess.text().upper()
 
-        UIFunctions.check_if_letters_in_word(self, guess)
+        UIFunctions.reset_colors(self)
         UIFunctions.set_previous_guess(self, guess)
-        UIFunctions.check_if_yellow(self, guess, letters_in_word)
+        UIFunctions.check_if_yellow(self, guess, UIFunctions.random_word)
         UIFunctions.check_if_green(self, guess, UIFunctions.random_word)
+        print(UIFunctions.random_word)
